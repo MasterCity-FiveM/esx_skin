3ff390441428e576f6d21e7fc3a4433d3545897a
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2021-05-10 03:27:53 +0400

    ...

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -23,16 +23,9 @@ server_scripts {
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
 	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/es.lua',
 	'config.lua',
-	'client/main.lua'
+	'client/*.lua'
 }
 
 dependencies {
